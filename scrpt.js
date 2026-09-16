788752e52cd2a7fb7b216d45c75033108538d065
Emon Ahmed <emon0860@gmail.com>
2026-09-16 22:45:30 +0600

    read task

--- scrpt.js
+++ scrpt.js
@@ -1,7 +1,18 @@
-const menuBtn = document.getElementById('menu-btn');
-    const mobileMenu = document.getElementById('mobile-menu');
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
 
-    menuBtn.addEventListener('click', () => {
-        mobileMenu.classList.toggle('hidden');
-        mobileMenu.classList.toggle('flex');
-    });
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+  mobileMenu.classList.toggle("flex");
+});
+
+document.getElementById("addTask").addEventListener("click", () => {
+  const task = document.getElementById("task").value;
+  const tag = document.querySelector("#tag").value;
+  const Priority = document.getElementById("Priority").value;
+  if (!task) {
+    return Swal.fire("please add task!");
+  } else {
+      console.log(tag, Priority, task);
+  }
+});
