f78f0f276aaeff459a0c9f9b1aecd5816170cb6c
Emon Ahmed <emon0860@gmail.com>
2026-09-19 17:27:01 +0600

    add toggle and delete function

--- scrpt.js
+++ scrpt.js
@@ -6,13 +6,149 @@ menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("flex");
 });
 
-document.getElementById("addTask").addEventListener("click", () => {
-  const task = document.getElementById("task").value;
-  const tag = document.querySelector("#tag").value;
-  const Priority = document.getElementById("Priority").value;
-  if (!task) {
+const taskInput = document.getElementById("task");
+const tag = document.querySelector("#tag");
+const Priority = document.getElementById("Priority");
+const taskContainer = document.getElementById("taskContainer");
+const addTask = document.getElementById("addTask");
+
+let tasks = [];
+let taskCount = 1;
+
+const addNewTask = () => {
+  let taskText = taskInput.value.trim();
+  let newTag = tag.value;
+  let newPriority = Priority.value;
+  if (!taskText) {
     return Swal.fire("please add task!");
-  } else {
-      console.log(tag, Priority, task);
+  }
+  const newTask = {
+    id: taskCount++,
+    taskText,
+    newTag,
+    newPriority,
+    complited: false,
+  };
+
+  tasks.push(newTask);
+  taskInput.value = "";
+  render();
+};
+
+const render = () => {
+  taskContainer.innerHTML = "";
+  if (taskContainer) {
+    let div = document.createElement("div");
+    div.innerHTML = `<div class="p-6 rounded-xl bg-[#fff] mt-8">
+            <div class="flex items-center justify-between">
+                <h2 class="text-black plus-jakarta-sans font-semibold"><i class="fa-regular fa-eye"></i> Empty State
+                    Pattern Preview</h2>
+                <span class="text-primary bg-[#E2E7FF] rounded-full text-xs font-medium px-2 py-0.5">Design Guide
+                    Mode</span>
+            </div>
+            <div class="bg-[#E2E7FF] mt-3 gap-3 flex flex-col items-center justify-center p-8 rounded-xl">
+                <span class="bg-[#fff] rounded-full p-4"><i class="fa-solid fa-list-check fa-xl text-primary"></i></span>
+                <h3 class="plus-jakarta-sans font-semibold text-xl">No Tasks Yet</h3>
+                <p class="text-sm text-dark">Add your first task and start getting things done. Your mind is for
+                    having ideas, not holding them.</p>
+                <button class="bg-primary text-white px-4 py-2 rounded-lg"><i class="fa-solid fa-plus"></i> Create First
+                    Task</button>
+            </div>
+        </div>`;
+  }
+  tasks.forEach((task) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="bg-[#fff] rounded-xl flex gap-4 p-3 items-center mb-2 justify-between">
+                <div class="flex gap-4 p-3 items-center mb-2"><input onchange="toggleTask(${task.id})" ${task.complited ? "checked" : ""} type="checkbox" name="" id=""
+                        class="rounded-sm cursor-pointer h-5 w-5 border border-[#C7C4D8]">
+                    <div>
+                        <h3 class="mb-1.5 font-medium text-sm ${task.complited ? "line-through opacity-50 text-gray-400" : "text-black"}">${task.taskText}</h3>
+                        <div class="flex items-center gap-2">
+                            <span
+                                class="text-primary bg-[#E2E7FF] py-0.5 px-2 rounded-full text-xs font-semibold">${task.newTag}</span>
+                            <span class="text-red text-xs font-semibold"><span
+                                    class="h-2 w-2 rounded-full inline-block mr-0.5 bg-red"></span>${task.newPriority}</span>
+                        </div>
+                    </div>
+                </div>
+                      <button onclick="deleteTsk(${task.id})" class="cursor-pointer">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+            </div>`;
+    taskContainer.appendChild(div);
+  });
+  console.log(tasks);
+};
+
+// Toggle Task Status
+const toggleTask = (id) => {
+  let task = tasks.find((task) => task.id === id);
+  task.complited = !task.complited;
+  render();
+};
+
+// Delete Task
+const deleteTsk = (id) => {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      tasks = tasks.filter((task) => task.id !== id);
+      render();
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your task has been deleted.",
+        icon: "success",
+      });
+    }
+  });
+};
+
+// EventListeners
+taskInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addNewTask();
   }
 });
+addTask.addEventListener("click", addNewTask);
+
+// const render = () => {
+//   if (!task) {
+//     return Swal.fire("please add task!");
+//   } else {
+//     const taskContainer = document.getElementById("taskContainer");
+//     const div = document.createElement("div");
+//     div.innerHTML = `<div class="bg-[#fff] rounded-xl flex gap-4 p-3 items-center mb-2 justify-between">
+//                 <div class="flex gap-4 p-3 items-center mb-2"><input type="checkbox" name="" id=""
+//                         class="rounded-sm cursor-pointer h-5 w-5 border border-[#C7C4D8]">
+//                     <div>
+//                         <h3 class="mb-1.5 text-black font-medium text-sm">${task}</h3>
+//                         <div class="flex items-center gap-2">
+//                             <span
+//                                 class="text-primary bg-[#E2E7FF] py-0.5 px-2 rounded-full text-xs font-semibold">${tag}</span>
+//                             <span class="text-red text-xs font-semibold"><span
+//                                     class="h-2 w-2 rounded-full inline-block mr-0.5 bg-red"></span>${Priority}</span>
+//                         </div>
+//                     </div>
+//                 </div>
+//                     <span class="cursor-pointer">
+//                         <i class="fa-solid fa-trash-can"></i>
+//                     </span>
+//             </div>`;
+
+//     taskContainer.appendChild(div);
+//     document.getElementById("task").value = ""
+//   }
+// }
+// document.getElementById("task").addEventListener("keydown", (e) => {
+//   if(e.key === "Enter"){
+//     render()
+//   }
+// })
+// document.getElementById("addTask").addEventListener("click", render);
